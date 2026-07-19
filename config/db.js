270e08153ea33eb2d9b7a9654f6094592e876eb3
Huligesh Malka <huligeshmalka@gmail.com>
2026-07-19 21:52:59 +0530

    Add TiDB SSL certificate

--- config/db.js
+++ config/db.js
@@ -4,27 +4,31 @@ const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 4000,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+
+    ssl: {
+        rejectUnauthorized: true
+    }
 });
 
 (async () => {
     try {
-
         const connection = await db.getConnection();
 
-        console.log("✅ MySQL Connected");
+        console.log("✅ TiDB Connected Successfully");
 
         connection.release();
 
     } catch (err) {
 
         console.error("❌ Database Connection Failed");
-
         console.error(err);
 
     }
