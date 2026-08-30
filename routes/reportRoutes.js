895075f775ff8c810459a6b1d482fe2f49323f48
Huligesh Malka <huligeshmalka@gmail.com>
2026-08-30 10:58:58 +0530

    Add vercel.json

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const reportController = require("../controllers/reportController");
 
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../middleware/auth");
 const businessMiddleware = require("../middleware/businessMiddleware");
 
 router.get(
